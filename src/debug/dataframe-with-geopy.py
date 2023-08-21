33929f723f3956ab9664db6f125095d8864c3b0b
dieissonmartins <dieisson.martins.santos@gmail.com>
2023-08-21 20:55:34 -0300

    commit with erro

---
+++ src/debug/dataframe-with-geopy.py
@@ -0,0 +1,41 @@
+from src.drivers.pyspark import Pyspark
+from pyspark.sql.functions import lit, col
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+from geopy.distance import geodesic
+from pyspark.sql.types import FloatType, IntegerType
+
+spark = Pyspark.start_session()
+
+# dataFrame com coordenadas
+data = [
+    ("Point A", -19.990224673, -44.00795550)
+]
+columns = ["point", "latitude", "longitude"]
+
+df = spark.createDataFrame(data, columns)
+
+
+# dunção UDF para calcular a distância entre coordenadas
+def calculate_distance(lat1, lng1, lat2, lng2):
+    coord1 = (lat1, lng1)
+    coord2 = (lat2, lng2)
+
+    ret = geodesic(coord1, coord2).meters
+
+    return ret
+
+
+calculate_distance_udf = udf(calculate_distance)
+
+lat2 = -19.99172504
+lon2 = -44.00998329
+
+# adicionar uma coluna com a distância entre as coordenadas
+df_with_distance = df.withColumn("distance", calculate_distance_udf(col('latitude'), col('longitude'), str(lat2)))
+
+# mostrar o DataFrame resultante
+df_with_distance.show()
+
+# encerrar a sessão Spark
+spark.stop()
